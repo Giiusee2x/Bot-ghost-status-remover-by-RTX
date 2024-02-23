0c3ee6a56f6e99f8269d6e7892fb57eae1f2daa3
Giiusee2x <161054088+Giiusee2x@users.noreply.github.com>
2024-02-23 16:38:19 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Developed By Giiusee1x","Sta Guardando Giiusee1x Shop"];
 
 
 let currentIndex = 0;
